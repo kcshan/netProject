9202318449898016687f010bfa03f71a031fda64
kcshan <cshan.kong@gmail.com>
2021-07-12 07:10:03 +0800

    first commit

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        // 程序的入口点
+        static void Main(string[] args)
+        {
+            // 在c#中，严格区分大小写；
+            Console.WriteLine("我们正在跟常老师学习C#");
+           
+            Console.Read();
+        }
+    }
+}
---
+++ MyCalModel/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyCalModel
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
