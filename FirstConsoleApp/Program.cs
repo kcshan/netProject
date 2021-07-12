9e2fe26e79a27e4b8bd106f472fb6d3d00dae91d
kcshan <cshan.kong@gmail.com>
2021-07-12 22:02:56 +0800

    FirstConsoleApp

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using VBCaluator;
+
 namespace FirstConsoleApp
 {
     class Program
@@ -13,7 +15,12 @@ namespace FirstConsoleApp
         {
             // 在c#中，严格区分大小写；
             Console.WriteLine("我们正在跟常老师学习C#");
-           
+
+            MyCal cal = new MyCal();
+            int result = cal.Add(10, 20);
+
+            Console.WriteLine("10 + 20 = " + result);
+
             Console.Read();
         }
     }
