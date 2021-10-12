7ad5b1fdcc9bac2772203b940bd3df5c585e7309
kcshan <cshan.kong@gmail.com>
2021-10-12 11:35:08 +0800

    feat: 1.0.2

---
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("这个是我的第一个C#程序！.NET神奇的编程世界从现在正式开始进入");
+            Console.Read();
+        }
+    }
+}
