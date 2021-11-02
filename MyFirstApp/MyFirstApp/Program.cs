afc18ff34848c266bcc245e188b70caa0ccdec3f
kcshan <cshan.kong@gmail.com>
2021-11-02 11:39:22 +0800

    feat: 0-4-11

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace MyFirstApp
 {
+    /*
+     * 这个是包含程序入口点的类
+     * 这个类里面的入口方法，不要随意修改
+     * 方法要求都是静态的
+     */
     class Program
     {
         static void Main(string[] args)
@@ -14,8 +19,22 @@ namespace MyFirstApp
             Console.WriteLine("请输入您的年龄：");
             string age = Console.ReadLine(); // 获取用户的输入
             Console.WriteLine("您刚才输入的年龄是："+age);
+
+            int result = add(10, 20);
+            Console.WriteLine("result: " + result);
             Console.ReadLine(); // 这个是让程序等待用户输入，暂停
             Console.Read();
+        }
+
+        /// <summary>
+        /// 俩个整数相加
+        /// </summary>
+        /// <param name="a">第一个整数</param>
+        /// <param name="b">第二个整数</param>
+        /// <returns></returns>
+        static int add(int a, int b)
+        {
+            return a + b;
         }
     }
 }
