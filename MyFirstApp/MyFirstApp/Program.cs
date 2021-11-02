91221b9d0175ccb1b47aa4011940401c1c577748
kcshan <cshan.kong@gmail.com>
2021-11-02 09:03:39 +0800

    feat: 0-4-8

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -11,6 +11,10 @@ namespace MyFirstApp
         static void Main(string[] args)
         {
             Console.WriteLine("这个是我的第一个C#程序！.NET神奇的编程世界从现在正式开始进入");
+            Console.WriteLine("请输入您的年龄：");
+            string age = Console.ReadLine(); // 获取用户的输入
+            Console.WriteLine("您刚才输入的年龄是："+age);
+            Console.ReadLine(); // 这个是让程序等待用户输入，暂停
             Console.Read();
         }
     }
--- MyFirstApp/WinFormAoo/Properties/AssemblyInfo.cs
+++ MyFirstApp/WinFormAoo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("MyFirstApp")]
-[assembly: AssemblyDescription("第一个项目")]
+[assembly: AssemblyTitle("WinFormAoo")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyFirstApp")]
+[assembly: AssemblyProduct("WinFormAoo")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("3ba4dfe2-cca5-4d49-9787-04c901cacbdf")]
+[assembly: Guid("826c9128-0e4e-4cb4-9e32-b697b37a6c6c")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
