734177538eb17d81d338f0024a2118b01d62dbdb
kcshan <cshan.kong@gmail.com>
2021-11-04 17:11:28 +0800

    feat: 0-4-1.2.4

--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -32,9 +32,42 @@ namespace Demo
             #endregion
 
             #region 枚举
-            Genders gender = Genders.Male;
-            Console.WriteLine((int)gender);
+            //Genders gender = Genders.Male;
+            //Console.WriteLine((int)gender);
 
+            #endregion
+
+            #region Console类1
+            //Console.WriteLine();
+            //Console.WriteLine("输出后换行");
+            //Console.Write("输出后不换行");
+            //Console.Write("输出后不换行");
+            #endregion
+
+            #region Console类2
+            //string stuName = "张三";
+            //int stuAge = 20;
+            //Console.WriteLine("学员姓名：" + stuName + " 年龄：" + stuAge);
+            //Console.WriteLine("学员姓名：{0} 年龄: {1}", stuName, stuAge);
+            #endregion
+
+            #region Console类3
+            //Console.WriteLine("请输入学员姓名：");
+            //string stuName = Console.ReadLine();
+
+            //Console.WriteLine("请输入学员年龄：");
+            //int stuAge = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("学员姓名：" + stuName + " 年龄：" + stuAge);
+            //Console.WriteLine("学员姓名：{0} 年龄: {1}", stuName, stuAge);
+            #endregion
+
+            #region Console类4
+            Console.WriteLine("姓名\t年龄\t班级\n");
+            Console.WriteLine("{0}\t{1}\t{2}", "张红1", 20, "软件1班");
+            Console.WriteLine("{0}\t{1}\t{2}", "张红2", 21, "软件2班");
+            Console.WriteLine("{0}\t{1}\t{2}", "张红3", 22, "软件3班");
+            Console.WriteLine("{0}\t{1}\t{2}", "张红4", 23, "软件4班");
             #endregion
 
             Console.ReadLine();
