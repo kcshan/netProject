e3caf122cb075459dccff3936c54a59177dbd1fe
kcshan <cshan.kong@gmail.com>
2021-12-25 14:29:54 +0800

    feat: 2.2.1: 方法的定义和使用

---
+++ DemeFunction/DemeFunction/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemeFunction
+{
+    class Student
+    {
+        //private int studentId;
+        public int StudentId { get; set; }
+
+        public string StudentName { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// 获取默认学员信息
+        /// </summary>
+        /// <returns></returns>
+        public string GetStudent()
+        {
+            string info = string.Format("姓名：{0} 学号：{1} 年龄：{2}", StudentName, StudentId, Age);
+            return info;
+        }
+
+        /// <summary>
+        /// 根据输入的学员信息返回详细内容
+        /// </summary>
+        /// <param name="stuName">学员姓名</param>
+        /// <param name="stuId">学号</param>
+        /// <param name="birthday">出生日期</param>
+        /// <returns>返回完整的学员信息</returns>
+        public string GetStudentInfo(string stuName, int stuId, DateTime birthday)
+        {
+            this.Birthday = birthday;
+            string info = string.Format("姓名：{0} 学号：{1} 生日：{2}", stuName, stuId, Birthday);
+            return info;
+        }
+
+        public void ShowStudentInfo()
+        {
+            string info = string.Format("姓名：{0} 学号：{1} 年龄：{2}", StudentName, StudentId, Age);
+            Console.WriteLine(info);
+        }
+    }
+}
---
+++ DemeFunction/DemeFunction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemeFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student objStu1 = new Student();
+            string info = objStu1.GetStudentInfo("肖明", 1002, Convert.ToDateTime("1990-06-30"));
+            Console.WriteLine(info);
+
+            Student objStu2 = new Student();
+            objStu2.StudentName = "黎明";
+            objStu2.StudentId = 1003;
+            objStu2.Birthday = Convert.ToDateTime("1992-06-21");
+            objStu2.ShowStudentInfo();
+            Console.ReadLine();
+        }
+    }
+}
