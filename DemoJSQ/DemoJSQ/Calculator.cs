205eed992426f3bf1534cd85db7a4cfddb55e6e9
kcshan <cshan.kong@gmail.com>
2021-12-27 14:15:33 +0800

    feat: 方法的重载

---
+++ DemoJSQ/DemoJSQ/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoJSQ
+{
+    class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Add(int a, double b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+        public double Sub(int a, double b)
+        {
+            return a - b;
+        }
+
+        public static int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
---
+++ DemoJSQ/DemoJSQ/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoJSQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator objCal = new Calculator();
+            int result1 = objCal.Add(1, 1);
+            double result2 = objCal.Add(1, 1.0);
+            int result3 = Calculator.Add(1, 2, 3);
+            Console.WriteLine("result1={0}", result1);
+            Console.WriteLine("result2={0}", result2);
+            Console.WriteLine("result3={0}", result3);
+            Console.ReadLine();
+        }
+    }
+}
