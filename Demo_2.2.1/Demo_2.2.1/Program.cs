8869bac69d1b973b15c3b99ce931dfd4a4ae72a5
kcshan <cshan.kong@gmail.com>
2022-03-16 13:24:47 +0800

    fix: 2.2.1

---
+++ Demo_2.2.1/Demo_2.2.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_2._2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student objStudent1 = new Student();
+            string info = objStudent1.GetStudentInfo("刘德华", 1002, Convert.ToDateTime("1990-10-10"));
+            Console.WriteLine(info);
+
+            Student objStudent2 = new Student();
+            objStudent2.StudentName = "黎明";
+            objStudent2.StudentId = 1003;
+            objStudent2.Birthday = Convert.ToDateTime("1992-10-12");
+            objStudent2.ShowStudentInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
