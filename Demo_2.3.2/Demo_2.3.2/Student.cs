8f5fefa295fe78710818d35eb2041bbf48227beb
kcshan <cshan.kong@gmail.com>
2022-03-17 20:54:08 +0800

    fix: 2.3.2

---
+++ Demo_2.3.2/Demo_2.3.2/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_2._3._2
+{
+    class Student
+    {
+        public Student()
+        {
+            this.StudentId = 10001;
+            this.StudentName = "张晓宇";
+        }
+        public Student(int stuId, string stuName)
+        {
+            this.StudentId = stuId;
+            this.StudentName = stuName;
+        }
+        public Student(int stuId, string stuName, DateTime birthday) : this(stuId, stuName)
+        {
+            this.Birthday = birthday;
+        }
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
