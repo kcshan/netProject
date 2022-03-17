3c0d2b80b4b53ecf7b93543e12e7214138419938
kcshan <cshan.kong@gmail.com>
2022-03-17 10:36:10 +0800

    fix: 2.2.4

---
+++ Demo_2.2.4/Demo_2.2.4/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_2._2._4
+{
+    class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Add(int a, double b)
+        {
+            return a + b;
+        }
+
+        public static int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Sub(int a, double b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ Demo_2.2.4/Demo_2.2.4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_2._2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator objCal = new Calculator();
+            int result1 = objCal.Add(1, 2);
+            double result2 = objCal.Add(1, 1.0);
+            int result3 = Calculator.Add(1, 2, 3);
+
+            Console.WriteLine("result1={0}", result1);
+            Console.WriteLine("result2={0}", result2);
+            Console.WriteLine("result3={0}", result3);
+
+            Console.ReadLine();
+        }
+    }
+}
