b308bb161de8ce4c6962c7f4abc416b133eff3e5
kcshan <cshan.kong@gmail.com>
2022-03-17 20:33:30 +0800

    fix: 2.3.1

---
+++ Demo_2.3.1/Demo_2.3.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_2._3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student objStudent = new Student();
+            Student objStudent1 = new Student(1002, "刘超");
+            Student objStudent2 = new Student(1001, "黎明", 18);
+
+            Console.WriteLine("姓名={0} 学号={1}", objStudent.StudentName, objStudent.StudentId);
+            Console.WriteLine("姓名={0} 学号={1}", objStudent1.StudentName, objStudent1.StudentId);
+            Console.WriteLine("姓名={0} 学号={1} 年龄={2}", objStudent2.StudentName, objStudent2.StudentId, objStudent2.Age);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Demo_2.3.1/Demo_2.3.1/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_2._3._1
+{
+    class Student
+    {
+        public Student()
+        {
+            this.StudentId = 10001;
+            this.StudentName = "张晓宇";
+        }
+        public Student(int stuId, string stuName)
+        {
+            this.StudentId = stuId;
+            this.StudentName = stuName;
+        }
+        public Student(int stuId, string stuName, int age): this(stuId, stuName)
+        {
+            this.Age = age;
+        }
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+}
