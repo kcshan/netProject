22aed767000341cbd40775d848146693a77cf147
kcshan <cshan.kong@gmail.com>
2022-03-21 16:01:28 +0800

    feat: 2.4.4

---
+++ Demo_2.4.4/Demo_2.4.4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_2._4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 创建几个学员对象
+            Student objStu1 = new Student(1001, "小王", 17);
+            Student objStu2 = new Student(1002, "小刘", 18);
+            Student objStu3 = new Student(1003, "小张", 16);
+            Student objStu4 = new Student(1004, "小李", 20);
+            List<Student> list = new List<Student>()
+            {
+                objStu1, objStu2, objStu3, objStu4
+            };
+            // 默认排序
+            Console.WriteLine("-----------默认排序-------------");
+            list.Sort();
+            foreach (Student item in list)
+            {
+                Console.WriteLine(item.StudentName + "\t" + item.StudentId + "\t" + item.Age);
+            }
+            Console.WriteLine("-----------按照姓名升序排序-------------");
+            list.Sort(new StuNameASC());
+            foreach (Student item in list)
+            {
+                Console.WriteLine(item.StudentName + "\t" + item.StudentId + "\t" + item.Age);
+            }
+            Console.WriteLine("-----------按照姓名降序排序-------------");
+            list.Sort(new StuNameDESC());
+            foreach (Student item in list)
+            {
+                Console.WriteLine(item.StudentName + "\t" + item.StudentId + "\t" + item.Age);
+            }
+
+            Console.WriteLine("-----------按照年龄降序排序-------------");
+            list.Sort(new AgeASC());
+            foreach (Student item in list)
+            {
+                Console.WriteLine(item.StudentName + "\t" + item.StudentId + "\t" + item.Age);
+            }
+            Console.WriteLine("-----------按照年龄降序排序-------------");
+            list.Sort(new AgeDESC());
+            foreach (Student item in list)
+            {
+                Console.WriteLine(item.StudentName + "\t" + item.StudentId + "\t" + item.Age);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Demo_2.4.4/Demo_2.4.4/Student.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_2._4._4
+{
+    class Student : IComparable<Student> // 默认排序，只能有一种
+    {
+        public Student() { }
+        public Student(int stuId, string stuName, int age)
+        {
+            this.StudentId = stuId;
+            this.StudentName = stuName;
+            this.Age = age;
+        }
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+
+
+        //int IComparable<Student>.CompareTo(Student other)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        /// <summary>
+        /// 接口的实现方法
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(Student other)
+        {
+            //throw new NotImplementedException();
+            // 降序
+            //return other.StudentId.CompareTo(this.StudentId);
+            // 升序
+            return this.StudentId.CompareTo(other.StudentId);
+        }
+    }
+
+    #region 排序类
+    // 添加4个排序类并且分别实现排序接口
+    class StuNameASC : IComparer<Student>
+    {
+        public int Compare(Student x, Student y)
+        {
+            return x.StudentName.CompareTo(y.StudentName);
+        }
+    }
+
+    class StuNameDESC : IComparer<Student>
+    {
+        public int Compare(Student x, Student y)
+        {
+            return y.StudentName.CompareTo(x.StudentName);
+        }
+    }
+
+    class AgeASC : IComparer<Student>
+    {
+        public int Compare(Student x, Student y)
+        {
+            return x.Age.CompareTo(y.Age);
+        }
+    }
+
+    class AgeDESC : IComparer<Student>
+    {
+        public int Compare(Student x, Student y)
+        {
+            return y.Age.CompareTo(x.Age);
+        }
+    }
+    #endregion
+}
