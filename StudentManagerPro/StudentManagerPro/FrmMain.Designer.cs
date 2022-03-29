0238ea215b52d554659459c391ab53c65dddadcf
kcshan <cshan.kong@gmail.com>
2022-03-29 11:40:33 +0800

    feat: 2.6.2

--- StudentManagerPro/StudentManagerPro/FrmMain.Designer.cs
+++ StudentManagerPro/StudentManagerPro/FrmMain.Designer.cs
@@ -28,21 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.系统ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmi_PwdModify = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.退出系统ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.学员管理MToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.添加学员AToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.批量导入学员ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.学员信息管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.系统ToolStripMenuItem,
+            this.学员管理MToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1264, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 系统ToolStripMenuItem
+            // 
+            this.系统ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmi_PwdModify,
+            this.toolStripSeparator1,
+            this.退出系统ToolStripMenuItem});
+            this.系统ToolStripMenuItem.Name = "系统ToolStripMenuItem";
+            this.系统ToolStripMenuItem.Size = new System.Drawing.Size(59, 21);
+            this.系统ToolStripMenuItem.Text = "系统(&S)";
+            // 
+            // tsmi_PwdModify
+            // 
+            this.tsmi_PwdModify.Image = ((System.Drawing.Image)(resources.GetObject("tsmi_PwdModify.Image")));
+            this.tsmi_PwdModify.Name = "tsmi_PwdModify";
+            this.tsmi_PwdModify.Size = new System.Drawing.Size(180, 22);
+            this.tsmi_PwdModify.Text = "密码修改(&M)";
+            this.tsmi_PwdModify.Click += new System.EventHandler(this.tsmi_PwdModify_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // 退出系统ToolStripMenuItem
+            // 
+            this.退出系统ToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("退出系统ToolStripMenuItem.Image")));
+            this.退出系统ToolStripMenuItem.Name = "退出系统ToolStripMenuItem";
+            this.退出系统ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.退出系统ToolStripMenuItem.Text = "退出系统";
+            // 
+            // 学员管理MToolStripMenuItem
+            // 
+            this.学员管理MToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.添加学员AToolStripMenuItem,
+            this.批量导入学员ToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.学员信息管理ToolStripMenuItem});
+            this.学员管理MToolStripMenuItem.Name = "学员管理MToolStripMenuItem";
+            this.学员管理MToolStripMenuItem.Size = new System.Drawing.Size(88, 21);
+            this.学员管理MToolStripMenuItem.Text = "学员管理(&M)";
+            // 
+            // 添加学员AToolStripMenuItem
+            // 
+            this.添加学员AToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("添加学员AToolStripMenuItem.Image")));
+            this.添加学员AToolStripMenuItem.Name = "添加学员AToolStripMenuItem";
+            this.添加学员AToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.添加学员AToolStripMenuItem.Text = "添加学员(&A)";
+            // 
+            // 批量导入学员ToolStripMenuItem
+            // 
+            this.批量导入学员ToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("批量导入学员ToolStripMenuItem.Image")));
+            this.批量导入学员ToolStripMenuItem.Name = "批量导入学员ToolStripMenuItem";
+            this.批量导入学员ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.批量导入学员ToolStripMenuItem.Text = "批量导入学员(&I)";
+            // 
+            // 学员信息管理ToolStripMenuItem
+            // 
+            this.学员信息管理ToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("学员信息管理ToolStripMenuItem.Image")));
+            this.学员信息管理ToolStripMenuItem.Name = "学员信息管理ToolStripMenuItem";
+            this.学员信息管理ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.学员信息管理ToolStripMenuItem.Text = "学员信息管理";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 707);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1264, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(81, 17);
+            this.toolStripStatusLabel1.Text = "版本号：V2.0";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(123, 17);
+            this.toolStripStatusLabel2.Text = "        [当前用户：***]";
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 729);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "FrmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "[学员信息管理系统]-最适合初学者实践的项目";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 系统ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmi_PwdModify;
+        private System.Windows.Forms.ToolStripMenuItem 退出系统ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem 学员管理MToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 添加学员AToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 批量导入学员ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 学员信息管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
     }
 }
 
--- StudentManagerPro/StudentManagerPro/FrmMain.cs
+++ StudentManagerPro/StudentManagerPro/FrmMain.cs
@@ -16,5 +16,11 @@ namespace StudentManagerPro
         {
             InitializeComponent();
         }
+
+        // 修改密码
+        private void tsmi_PwdModify_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
