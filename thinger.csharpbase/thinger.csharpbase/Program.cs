0af1af690a6ce791d9aebe2ecaaa5eb19e3f6cde
kcshan <cshan.kong@gmail.com>
2022-04-20 07:50:07 +0800

    feat: 1-4-2

--- thinger.csharpbase/thinger.csharpbase/Program.cs
+++ thinger.csharpbase/thinger.csharpbase/Program.cs
@@ -18,7 +18,9 @@ namespace thinger.csharpbase
 
             int result = Add(10,20);
 
-            Test2();
+            //Test2();
+
+            Test3();
 
             Console.ReadLine();
         }
@@ -68,6 +70,31 @@ namespace thinger.csharpbase
             Console.WriteLine("姓名：" + name + " 成绩：" + score);
 
         }
+
+        // 字符串格式化方法和控制台输出格式化
+        static void Test3()
+        {
+            Console.Write("请输入学员姓名：");
+            string name = Console.ReadLine();
+            Console.Write("请输入学员成绩：");
+            int score = int.Parse(Console.ReadLine()); // 90
+            Console.Write("请输入班级：");
+            string className = Console.ReadLine();
+
+            // 使用 + 拼接字符串
+            string info1 = "姓名：" + name + " 成绩：" + score;
+
+            // 使用占位符拼接字符串
+            string info2 = string.Format("姓名：{0} 成绩：{1} 班级：{2}", name, score, className);
+
+            Console.WriteLine(info2);
+            Console.WriteLine();
+
+            // WriteLine方法支持直接的格式化方法
+            Console.WriteLine("姓名：{0} 成绩：{1} 班级：{2}", name, score, className);
+        }
         #endregion
+
+
     }
 }
