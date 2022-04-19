f270b864a808e0124fedaa8abd6aa318847c6b4f
kcshan <cshan.kong@gmail.com>
2022-04-19 20:34:40 +0800

    feat: 1-3-7

--- thinger.csharpbase/thinger.csharpbase/Program.cs
+++ thinger.csharpbase/thinger.csharpbase/Program.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace thinger.csharpbase
 {
+    /// <summary>
+    /// 这个是控制台核心类
+    /// 主要是供我们练习使用
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
             Test1();
+
+            int result = Add(10,20);
+
+            Console.Read();
         }
 
         static void Test1()
@@ -25,6 +33,17 @@ namespace thinger.csharpbase
             Console.WriteLine("科目：" + subject + " 分数：" + score);
 
             Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 实现俩个数相加
+        /// </summary>
+        /// <param name="a">第一个整数</param>
+        /// <param name="b">第二个整数</param>
+        /// <returns>返回俩个数的和</returns>
+        static int Add(int a, int b)
+        {
+            return a + b;
         }
     }
 }
