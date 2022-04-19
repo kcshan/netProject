10f8d3cf5889c380749ce073a98618bdcd24e523
kcshan <cshan.kong@gmail.com>
2022-04-19 17:13:41 +0800

    feat: 1-3-4

---
+++ thinger.csharpbase/thinger.csharpbase/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thinger.csharpbase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1();
+        }
+
+        static void Test1()
+        {
+            int score; // 变量类型 变量名称
+
+            score = 98; // 给变量赋值
+
+            string subject = "C#基础语法"; // 声明变量同时赋值
+
+            // 使用变量
+            Console.WriteLine("科目：" + subject + " 分数：" + score);
+
+            Console.ReadLine();
+        }
+    }
+}
