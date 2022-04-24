bdd147e506832098ba8da32dda29e570f5fa8ee1
kcshan <cshan.kong@gmail.com>
2022-04-24 14:34:20 +0800

    feat: 1-12-4

---
+++ xiketang.com.MethodApp/xiketang.com.MethodApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xiketang.com.MethodApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass testClass = new TestClass();
+            testClass.Dowork1();
+            testClass.Dowork2();
+
+            Console.ReadLine();
+        }
+    }
+}
