9b11e0c1cec917b929a3103929f55f63314d31e3
kcshan <cshan.kong@gmail.com>
2022-04-25 20:26:18 +0800

    feat: 1-14-1

---
+++ xiketang.com.WinformBase/xiketang.com.WinformBase/FrmMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xiketang.com.WinformBase
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 构造方法：初始化所有的控件
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent(); // 调用后面的Desinger类中的方法，用于控件初始化
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ xiketang.com.WinformBase/xiketang.com.WinformBase/MyForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xiketang.com.WinformBase
+{
+    class MyForm: Form
+    {
+        private Button button1;
+
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MyForm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Name = "MyForm";
+            this.ResumeLayout(false);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
