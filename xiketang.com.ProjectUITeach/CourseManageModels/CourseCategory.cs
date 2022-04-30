1eb7cd1b493b170b19ade74c752acffbbf62b389
kcshan <cshan.kong@gmail.com>
2022-05-01 07:49:29 +0800

    feat: 1-20-1

---
+++ xiketang.com.ProjectUITeach/CourseManageModels/CourseCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManageDAL
+{
+    /// <summary>
+    /// 课程分类实体类
+    /// </summary>
+    public class CourseCategory
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
