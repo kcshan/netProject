38b33584d108d25923bcb9b6587e84f5df511514
kcshan <cshan.kong@gmail.com>
2022-08-05 13:01:07 +0800

    fix

---
+++ Demo2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 创建对象
+            Student objStudent = new Student();
+
+            // 给对象属性赋值
+            objStudent.StudentId = 10001;
+            objStudent.StudentName = "王丽丽";
+
+            String info = objStudent.GetStudent();
+            Console.WriteLine(info);
+            Console.ReadLine();
+        }
+    }
+}
